c399eaa60e2470235066fd7ba1dafd587737962d
KasumiMercury <naoki_shira@outlook.jp>
2025-07-13 20:39:30 +0900

    terraform for Artifact Registry

---
+++ terraform/registry/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "A name of a GCP project"
+  type        = string
+}
+
+variable "location" {
+  description = "A location of a cloud run instance"
+  type        = string
+}
