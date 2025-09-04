d93105534b16d914727828b0517f954a6e8a9faa
KasumiMercury <naoki_shira@outlook.jp>
2025-09-04 22:07:57 +0900

    fix: mark Cloud Run outputs as sensitive

--- terraform/modules/cloud-run/outputs.tf
+++ terraform/modules/cloud-run/outputs.tf
@@ -1,16 +1,19 @@
 output "service_name" {
   description = "The name of the Cloud Run service"
   value       = google_cloud_run_v2_service.default.name
+  sensitive   = true
 }
 
 output "service_url" {
   description = "The URL of the Cloud Run service"
   value       = google_cloud_run_v2_service.default.uri
+  sensitive   = true
 }
 
 output "urls" {
   description = "The URLs on which the deployed service is available"
   value       = google_cloud_run_v2_service.default.urls
+  sensitive   = true
 }
 
 output "location" {
