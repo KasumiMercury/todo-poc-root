8ddc78326b8a629d8c016b1b4dda500f7add197d
KasumiMercury <naoki_shira@outlook.jp>
2025-09-17 18:28:05 +0900

    fix: add missing output for workload identity pool ID

--- terraform/modules/workload-identity/main.tf
+++ terraform/modules/workload-identity/main.tf
@@ -20,7 +20,7 @@ resource "google_iam_workload_identity_pool_provider" "github" {
   workload_identity_pool_provider_id = var.provider_id
   display_name                       = var.provider_display_name
   description                        = var.provider_description
-  
+
   attribute_condition = var.attribute_condition
 
   attribute_mapping = var.attribute_mapping
--- terraform/modules/workload-identity/outputs.tf
+++ terraform/modules/workload-identity/outputs.tf
@@ -3,6 +3,11 @@ output "workload_identity_pool_name" {
   value       = google_iam_workload_identity_pool.github.name
 }
 
+output "workload_identity_pool_id" {
+  description = "The workload identity pool ID"
+  value       = google_iam_workload_identity_pool.github.workload_identity_pool_id
+}
+
 output "workload_identity_pool_provider_name" {
   description = "The name of the workload identity pool provider"
   value       = google_iam_workload_identity_pool_provider.github.name
