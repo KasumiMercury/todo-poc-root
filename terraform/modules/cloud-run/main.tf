405e1927e4a4025bb9c4e47f52d996807ed8703b
KasumiMercury <naoki_shira@outlook.jp>
2025-09-17 18:28:46 +0900

    style: format

--- terraform/modules/cloud-run/main.tf
+++ terraform/modules/cloud-run/main.tf
@@ -12,11 +12,11 @@ resource "google_cloud_run_v2_service" "default" {
   name     = var.service_name
   location = var.location
   project  = var.project_id
-  
+
   template {
     containers {
       image = var.container_image
-      
+
       dynamic "env" {
         for_each = var.environment_variables
         content {
@@ -27,14 +27,14 @@ resource "google_cloud_run_v2_service" "default" {
     }
     service_account = var.runtime_service_account_email
   }
-  
+
   deletion_protection = var.deletion_protection
 }
 
 # Optional: Allow unauthenticated access if specified
 resource "google_cloud_run_v2_service_iam_member" "public_access" {
   count = var.allow_unauthenticated_access ? 1 : 0
-  
+
   project  = var.project_id
   location = google_cloud_run_v2_service.default.location
   name     = google_cloud_run_v2_service.default.name
